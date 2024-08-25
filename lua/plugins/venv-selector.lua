fb8db4099c697da2ae6eaee1e32f3cc8c123a8a8
ConnorGlynn13 <connorglynn13@GMAIL.COM>
2024-08-25 11:25:21 +0100

    🆙 Update `VenvSelect` to `regexp` version

--- lua/plugins/venv-selector.lua
+++ lua/plugins/venv-selector.lua
@@ -1,15 +1,17 @@
 return {
   "linux-cultist/venv-selector.nvim",
-  branch = "main", -- Use this branch for the new version
-  cmd = "VenvSelect",
-  enabled = function()
-	  return require("lazy.core.config").plugins["telescope.nvim"] ~= nil
-  end,
+  branch = "regexp",
+  -- cmd = "VenvSelect",
+  -- enabled = function()
+  --  return require("lazy.core.config").plugins["telescope.nvim"] ~= nil
+  -- end,
   opts = {
     settings = {
       options = {
         notify_user_on_venv_activation = true,
         pipenv_path = "/Users/connor.glynn/.local/share/virtualenvs",
+        pipenv_auto_detection = true, -- Automatically detect Pipenv files
+        auto_activate = true, -- Automatically activate the Pipenv environment
       },
     },
   },
