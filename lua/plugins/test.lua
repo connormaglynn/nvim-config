f21b318f731d3764d188cbc527a0975be5746d60
connormaglynn <connor.glynn@digital.justice.gov.uk>
2024-10-08 07:25:43 +0100

    ♻ Refactor `neotest` to use `opts`

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -3,15 +3,13 @@ return {
   dependencies = {
     "neotest-python",
   },
-  config = function()
-    require("neotest").setup({
-      adapters = {
-        require("neotest-python")({
-          dap = { justMyCode = false },
-          args = { "--log-level", "DEBUG" },
-          runner = "pytest",
-        }),
+  opts = {
+    adapters = {
+      ["neotest-python"] = {
+        dap = { justMyCode = false },
+        args = { "--log-level", "DEBUG" },
+        runner = "pytest",
       },
-    })
-  end,
+    },
+  },
 }
