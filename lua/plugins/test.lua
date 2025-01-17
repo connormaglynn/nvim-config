3206635fa9d66f191c5a75e0e6a4f4ebdcb1c533
connormaglynn <connor.glynn@digital.justice.gov.uk>
2025-01-17 13:16:55 +0000

    🔧 Fix Neotest

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -1,15 +1,23 @@
 return {
   "nvim-neotest/neotest",
   dependencies = {
-    "neotest-python",
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    "antoinemadec/FixCursorHold.nvim", -- Required for Neotest
+    "nvim-neotest/neotest-python", -- Python adapter for Neotest
   },
-  opts = {
-    adapters = {
-      ["neotest-python"] = {
-        dap = { justMyCode = false },
-        args = { "--log-level", "DEBUG" },
-        runner = "pytest",
+  opts = function()
+    return {
+      adapters = {
+        require("neotest-python")({
+          dap = { justMyCode = false },
+          args = { "--log-level", "DEBUG" },
+          runner = "pytest",
+        }),
       },
-    },
-  },
+    }
+  end,
+  config = function(_, opts)
+    require("neotest").setup(opts)
+  end,
 }
--- lua/plugins/venv-selector.lua
+++ lua/plugins/venv-selector.lua
@@ -1,13 +1,37 @@
+-- return {
+--   "linux-cultist/venv-selector.nvim",
+--   opts = {
+--     settings = {
+--       options = {
+--         notify_user_on_venv_activation = true,
+--         pipenv_path = "~/.local/share/virtualenvs",
+--         pipenv_auto_detection = true, -- Automatically detect Pipenv files
+--         auto_activate = true, -- Automatically activate the Pipenv environment
+--       },
+--     },
+--   },
+-- }
+
 return {
   "linux-cultist/venv-selector.nvim",
+  dependencies = {
+    "neovim/nvim-lspconfig",
+    "mfussenegger/nvim-dap",
+    "mfussenegger/nvim-dap-python", --optional
+    { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+  },
+  lazy = false,
+  branch = "regexp", -- This is the regexp branch, use this for the new version
   opts = {
-    settings = {
-      options = {
-        notify_user_on_venv_activation = true,
-        pipenv_path = "~/.local/share/virtualenvs",
-        pipenv_auto_detection = true, -- Automatically detect Pipenv files
-        auto_activate = true, -- Automatically activate the Pipenv environment
-      },
-    },
+    notify_user_on_venv_activation = true,
+    pipenv_path = vim.fn.expand("~/.local/share/virtualenvs"), -- Automatically expand ~
+    pipenv_auto_detection = true,
+    auto_activate = true,
+  },
+  config = function()
+    require("venv-selector").setup()
+  end,
+  keys = {
+    { ",v", "<cmd>VenvSelect<cr>" },
   },
 }
