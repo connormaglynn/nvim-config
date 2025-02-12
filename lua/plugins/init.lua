3eb1900bc3dbcc212946ab71154a8f40eeb5bddf
connormaglynn <connor.glynn@digital.justice.gov.uk>
2025-02-12 12:53:40 +0000

    ✨ Add Some Basic Plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,11 +1,53 @@
 return {
-	'nvim-telescope/telescope.nvim',
-	dependencies = { 'nvim-lua/plenary.nvim' },
-	config = function()
-		local builtin = require('telescope.builtin')
-		vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-		vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-		vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-		vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-	end,
+
+	{
+		"Mofiqul/vscode.nvim"
+	},
+
+	{
+		'nvim-telescope/telescope.nvim',
+		dependencies = { 'nvim-lua/plenary.nvim' },
+		config = function()
+			local builtin = require('telescope.builtin')
+			vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+			vim.keymap.set('n', '<leader><leader>', builtin.find_files, { desc = 'Telescope find files' })
+			vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+			vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+			vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+		end,
+	},
+
+	{
+		'folke/which-key.nvim',
+		dependencies = { 'echasnovski/mini.icons', 'nvim-tree/nvim-web-devicons' },
+		event = 'VeryLazy',
+		keys = {
+			{
+				'<leader>?',
+				function()
+					require('which-key').show({ global = false })
+				end,
+				desc = 'Buffer Local Keymaps (which-key)',
+			},
+		},
+	},
+
+
+	{
+		"kdheepak/lazygit.nvim",
+		lazy = true,
+		cmd = {
+			"LazyGit",
+			"LazyGitConfig",
+			"LazyGitCurrentFile",
+			"LazyGitFilter",
+			"LazyGitFilterCurrentFile",
+		},
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		keys = {
+			{ "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+		}
+	}
 }
