fdb061bbebf761399286704d6b17fa7720b677e7
connormaglynn <connor.glynn@justice.gov.uk>
2025-10-12 18:47:02 +0100

    🎨 Update Colour Scheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+  { "ellisonleao/gruvbox.nvim" },
+  { "savq/melange-nvim" },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+}
